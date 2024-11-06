124e79dc44cd914bb41eacb7f65265bcffe397c2
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-11-06 17:38:56 +0100

    adding description, min, and max

--- js/script.js
+++ js/script.js
@@ -7,12 +7,15 @@ async function fetchCapitals() {
     const countries = await response.json();
 
     countries.forEach(country => {
-        if (country.capital && country.flags) {
+        if (country.capital && country.flags && country.capital != 'Jerusalem') {
             let option = document.createElement('div');
             option.classList.add('dropdown-item');
             // store the selected capital city’s name within each dropdown item
             option.setAttribute('data-capital', country.capital[0]);
-            option.innerHTML = `${country.name.common} - ${country.capital[0]} <img src='${country.flags.png}' width='20px' height='15px'>`;
+            option.innerHTML = `
+            <p>${country.name.common} - ${country.capital[0]}</p>
+            <img src='${country.flags.png}' width='20px' height='15px'>
+            `;
             select.append(option);
 
             option.onclick = () => selectCity(country.capital[0]);
@@ -33,7 +36,7 @@ function toggleDropdown() {
 
 async function getWeather() {
     if (!selectedCity) return alert('Please select a city first');
-    
+
     const apiKey = '9060bd1d3a6fb963d29e880953d842ed';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`;
 
@@ -41,6 +44,8 @@ async function getWeather() {
         const response = await fetch(apiUrl);
         if (!response.ok) throw new Error("City not found");
         const data = await response.json();
+        console.log(data);
+
         displayWeather(data);
     } catch (error) {
         document.getElementById('weatherDisplay').innerHTML = `<p>${error.message}</p>`;
@@ -49,10 +54,14 @@ async function getWeather() {
 
 function displayWeather(data) {
     const weatherDisplay = document.getElementById('weatherDisplay');
-    const { name, main } = data;
+    const { name, main, weather } = data;
+    const description = weather[0].description;
     weatherDisplay.innerHTML = `
         <h2>${name}</h2>
-        <p>Temperature: ${main.temp}°C</p>
+        <p>${Math.round(main.temp)}°</p>
+        <p>${description.charAt(0).toUpperCase() + description.slice(1)} </p>
+        <p>H:${Math.round(main.temp_max)}°   L:${Math.round(main.temp_min)}°</p>
+
     `;
 }
 
