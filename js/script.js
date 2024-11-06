41e4cada706352a99bba8832e126b51630bc4540
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2024-11-06 16:37:38 +0100

    creating a dropdown that allows images next to text

--- js/script.js
+++ js/script.js
@@ -1,25 +1,60 @@
+const select = document.getElementById('select');
+let selectedCity = '';
+
+
+async function fetchCapitals() {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const countries = await response.json();
+
+    countries.forEach(country => {
+        if (country.capital && country.flags) {
+            let option = document.createElement('div');
+            option.classList.add('dropdown-item');
+            // store the selected capital city’s name within each dropdown item
+            option.setAttribute('data-capital', country.capital[0]);
+            option.innerHTML = `${country.name.common} - ${country.capital[0]} <img src='${country.flags.png}' width='20px' height='15px'>`;
+            select.append(option);
+
+            option.onclick = () => selectCity(country.capital[0]);
+        }
+    });
+}
+
+function selectCity(city) {
+    selectedCity = city;
+    document.querySelector('.dropdown-btn').textContent = city;
+    toggleDropdown()
+}
+
+function toggleDropdown() {
+    select.classList.toggle('show');
+}
+
+
 async function getWeather() {
-    let cityInput = document.getElementById('cityInput').value;
-    let apiKey = '9060bd1d3a6fb963d29e880953d842ed';
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
+    if (!selectedCity) return alert('Please select a city first');
+    
+    const apiKey = '9060bd1d3a6fb963d29e880953d842ed';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${apiKey}&units=metric`;
 
     try {
         const response = await fetch(apiUrl);
-        if (! response.ok) throw new Error("city not found");
+        if (!response.ok) throw new Error("City not found");
         const data = await response.json();
-        // console.log(data);
-        displayWeather(data); 
+        displayWeather(data);
     } catch (error) {
         document.getElementById('weatherDisplay').innerHTML = `<p>${error.message}</p>`;
     }
 }
 
 function displayWeather(data) {
-    weatherDisplay = document.getElementById('weatherDisplay');
-    const {name, main} = data;
+    const weatherDisplay = document.getElementById('weatherDisplay');
+    const { name, main } = data;
     weatherDisplay.innerHTML = `
         <h2>${name}</h2>
-        <p>${main.temp}°C</p>
+        <p>Temperature: ${main.temp}°C</p>
     `;
-    
 }
+
+// Load capitals on page load
+window.onload = fetchCapitals;
